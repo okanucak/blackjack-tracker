4a5fde539d70e75cb61f0e135beedb69a311f37d
okanucak <okanucak@hotmail.com>
2025-04-09 11:21:08 +0300

    Deck count made dynamic.

--- js/defines.js
+++ js/defines.js
@@ -3,117 +3,15 @@
  */
 
 // Constants
-const DECK_COUNT = 8;
+const DECK_COUNTS = [1, 2, 4, 6, 8];
 const HIGHT = "hight";
 const LOW = "low";
 
 // Variables
+var deckCount = 6;
+var cards = [];
 var dealerCards = [];
 var playerCards = [];
 var otherCards = [];
-var pendingCardCount = DECK_COUNT * 52;
+var pendingCardCount = deckCount * 52;
 var hiloNumber = 0;
-
-// Card List
-var cards = [
-    {
-        realValue: 2,
-        displayValue: "2",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: LOW,
-    },
-    {
-        realValue: 3,
-        displayValue: "3",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: LOW,
-    },
-    {
-        realValue: 4,
-        displayValue: "4",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: LOW,
-    },
-    {
-        realValue: 5,
-        displayValue: "5",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: LOW,
-    },
-    {
-        realValue: 6,
-        displayValue: "6",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: LOW,
-    },
-    {
-        realValue: 7,
-        displayValue: "7",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: null,
-    },
-    {
-        realValue: 8,
-        displayValue: "8",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: null,
-    },
-    {
-        realValue: 9,
-        displayValue: "9",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: null,
-    },
-    {
-        realValue: 10,
-        displayValue: "10",
-        totalAmount: 4 * DECK_COUNT * 4,
-        pendingAmount: 4 * DECK_COUNT * 4,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: HIGHT,
-    },
-    {
-        realValue: 11,
-        displayValue: "A",
-        totalAmount: 4 * DECK_COUNT,
-        pendingAmount: 4 * DECK_COUNT,
-        completedAmount: 0,
-        playerRate: 0,
-        dealerRate: 0,
-        hilo: HIGHT,
-    },
-];
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,111 @@
-function newDeck() {
-	for (var i = 0; i < cards.length; i++) {
-		cards[i].pendingAmount = cards[i].totalAmount;
-		cards[i].completedAmount = 0;
-	}
-	pendingCardCount = DECK_COUNT * 52;
+function suffle() {
+	cards = [
+		{
+			realValue: 2,
+			displayValue: "2",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: LOW,
+		},
+		{
+			realValue: 3,
+			displayValue: "3",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: LOW,
+		},
+		{
+			realValue: 4,
+			displayValue: "4",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: LOW,
+		},
+		{
+			realValue: 5,
+			displayValue: "5",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: LOW,
+		},
+		{
+			realValue: 6,
+			displayValue: "6",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: LOW,
+		},
+		{
+			realValue: 7,
+			displayValue: "7",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: null,
+		},
+		{
+			realValue: 8,
+			displayValue: "8",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: null,
+		},
+		{
+			realValue: 9,
+			displayValue: "9",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: null,
+		},
+		{
+			realValue: 10,
+			displayValue: "10",
+			totalAmount: 4 * deckCount * 4,
+			pendingAmount: 4 * deckCount * 4,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: HIGHT,
+		},
+		{
+			realValue: 11,
+			displayValue: "A",
+			totalAmount: 4 * deckCount,
+			pendingAmount: 4 * deckCount,
+			completedAmount: 0,
+			playerRate: 0,
+			dealerRate: 0,
+			hilo: HIGHT,
+		},
+	];
+
+	pendingCardCount = deckCount * 52;
+	
 	hiloNumber = 0;
+	
 	newTour();
 }
 
